92df66517995432905cc9d98dfcbbdb7652e0bdf
pcidviayna <pablocidviayna@gmail.com>
2025-11-17 00:19:31 +0100

    versio 1.1

--- pablo_exh.py
+++ pablo_exh.py
@@ -1,6 +1,6 @@
 from yogi import *
 from time import time
-from sys import maxsize
+import sys
 
 def nou_cost(sol: list[int], millores:list[list[int]], idx: int, ce: list[int], ne:list[int]) -> int:
     M = len(ce)
@@ -15,13 +15,23 @@ def nou_cost(sol: list[int], millores:list[list[int]], idx: int, ce: list[int], 
         nou_cost += sum(max((n-1)*(n)//2, 0) for n in dif)
     return nou_cost
 
-def min_pen_rec(sol: list[int], classes_restants: list[int], millores: list[list[int]], cost_actual: int, idx: int, ce: list[int], ne: list[int], min_cost: list[int], inici: int):
+def min_pen_rec(sol: list[int], classes_restants: list[int], millores: list[list[int]], cost_actual: int, idx: int, ce: list[int], ne: list[int], min_cost: list[int], inici: float):
     C = len(sol)
     M = len(millores[0])
     K = len(millores)
     if idx == C:
         if cost_actual < min_cost[0]:
             min_cost[0] = cost_actual
+            try:
+                with open(sys.argv[1],"w") as f: 
+                    final = time()
+                    print(cost_actual, round(final - inici,1), file=f)
+                    print(' '.join(map(str, sol)), file=f)
+                return cost_actual, sol.copy()
+            except IndexError:
+                print("Error. No s'ha rebut cap fitxer de sortida")
+                min_cost[0] = -1
+                return -1, sol
         return cost_actual, sol.copy()
     elif cost_actual >= min_cost[0]:
         return cost_actual, sol
@@ -31,11 +41,10 @@ def min_pen_rec(sol: list[int], classes_restants: list[int], millores: list[list
         for i in range(K):
             if classes_restants[i] != 0:
                 nou_cotxe = i
-                print(classes_restants, sol, idx)
                 classes_restants[i] -= 1
                 sol[idx] = nou_cotxe
                 nc = nou_cost(sol, millores, idx, ce, ne)
-                cost, new_sol = min_pen_rec(sol, classes_restants, millores, cost_actual+nc, idx+1, ce, ne, min_cost)
+                cost, new_sol = min_pen_rec(sol, classes_restants, millores, cost_actual+nc, idx+1, ce, ne, min_cost, inici)
                 if cost < best_cost:
                     best_cost = cost
                     best_sol = new_sol
@@ -43,13 +52,12 @@ def min_pen_rec(sol: list[int], classes_restants: list[int], millores: list[list
                 classes_restants[i] += 1 
         return best_cost, best_sol
     
-def min_pen(C: int, cotxes_classe: list[int], millores: list[list[int]], ce: list[int], ne: list[int]):
+def min_pen(C: int, cotxes_classe: list[int], millores: list[list[int]], ce: list[int], ne: list[int], inici: float):
     sol = [-1]*C
     classes_restants = cotxes_classe.copy()
     cost_actual = 0
     idx = 0
     min_cost = [sys.maxsize]
-    inici = time.time()
     best_cost, best_sol = min_pen_rec(sol, classes_restants, millores, cost_actual, idx, ce, ne, min_cost, inici)
     return best_cost, best_sol
 
@@ -70,8 +78,11 @@ def read_input() -> tuple[int, list[int], list[list[int]], list[int], list[int]]
 
 def main():
     C, cotxes_classe, millores, ce, ne = read_input()
-    cost, sol= min_pen(C, cotxes_classe, millores, ce, ne)
-    print(cost, sol)
+    inici = time()
+    cost, sol= min_pen(C, cotxes_classe, millores, ce, ne, inici)
+    final = time()
+    print(cost, round(final - inici,1))
+    print(' '.join(map(str, sol)))
 
 main()
 
