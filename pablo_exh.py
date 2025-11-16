0fa2583799962e8e0c34d938852312a4406888d9
pcidviayna <pablocidviayna@gmail.com>
2025-11-16 23:38:26 +0100

    "Versio 1.0"

--- pablo_exh.py
+++ pablo_exh.py
@@ -1,8 +1,80 @@
-from yogi import read
+from yogi import *
 from time import time
+from sys import maxsize
 
-def main():
+def nou_cost(sol: list[int], millores:list[list[int]], idx: int, ce: list[int], ne:list[int]) -> int:
+    M = len(ce)
+    nou_cost = 0
+    estacions_ocupades = [0]*M
+    for i in range(M):
+        for j in range(idx, max(-1, idx-ne[i]), -1):
+            estacions_ocupades[i] += millores[sol[j]][i]
+    dif = [max(0, estacions_ocupades[i]-ce[i]) for i in range(M)]
+    nou_cost += sum(dif)
+    if idx == len(sol) - 1:
+        nou_cost += sum(max((n-1)*(n)//2, 0) for n in dif)
+    return nou_cost
+
+def min_pen_rec(sol: list[int], classes_restants: list[int], millores: list[list[int]], cost_actual: int, idx: int, ce: list[int], ne: list[int], min_cost: list[int], inici: int):
+    C = len(sol)
+    M = len(millores[0])
+    K = len(millores)
+    if idx == C:
+        if cost_actual < min_cost[0]:
+            min_cost[0] = cost_actual
+        return cost_actual, sol.copy()
+    elif cost_actual >= min_cost[0]:
+        return cost_actual, sol
+    else:
+        best_cost = sys.maxsize
+        best_sol = [-1]*C
+        for i in range(K):
+            if classes_restants[i] != 0:
+                nou_cotxe = i
+                print(classes_restants, sol, idx)
+                classes_restants[i] -= 1
+                sol[idx] = nou_cotxe
+                nc = nou_cost(sol, millores, idx, ce, ne)
+                cost, new_sol = min_pen_rec(sol, classes_restants, millores, cost_actual+nc, idx+1, ce, ne, min_cost)
+                if cost < best_cost:
+                    best_cost = cost
+                    best_sol = new_sol
+                sol[idx] = -1
+                classes_restants[i] += 1 
+        return best_cost, best_sol
+    
+def min_pen(C: int, cotxes_classe: list[int], millores: list[list[int]], ce: list[int], ne: list[int]):
+    sol = [-1]*C
+    classes_restants = cotxes_classe.copy()
+    cost_actual = 0
+    idx = 0
+    min_cost = [sys.maxsize]
+    inici = time.time()
+    best_cost, best_sol = min_pen_rec(sol, classes_restants, millores, cost_actual, idx, ce, ne, min_cost, inici)
+    return best_cost, best_sol
+
+
+def read_input() -> tuple[int, list[int], list[list[int]], list[int], list[int]]:
     C = read(int)
     M = read(int)
     K = read(int)
+    ce = [read(int) for _ in range(M)]
+    ne = [read(int) for _ in range(M)]
+    cotxes_classe = [-1]*K
+    millores = [[-1]*M for _ in range(K)]
+    for _ in range(K):
+        i = read(int)
+        cotxes_classe[i] = read(int)
+        millores[i] = [read(int) for _ in range (M)]
+    return C, cotxes_classe, millores, ce, ne
+
+def main():
+    C, cotxes_classe, millores, ce, ne = read_input()
+    cost, sol= min_pen(C, cotxes_classe, millores, ce, ne)
+    print(cost, sol)
+
+main()
+
     
+
+
